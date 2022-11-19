77cfea83007b037257312b4104db2bf431b51240
Ikraan Ahmed <109916253+Ikraan@users.noreply.github.com>
2022-11-19 14:43:17 +0000

    updated modal

--- js/index.js
+++ js/index.js
@@ -1,73 +1,32 @@
 import { dates } from "./data.js";
-
 //return the timeline class element from the html file
 const timeline = document.querySelector(".timeline");
 
-function getCard(index) {
-  const { date, title, image, fullDescription } = dates[index];
-  const wrapper = document.createElement("div");
-  const cardHeader = document.createElement("div");
-  const timelineItemSummary = document.createElement("div");
-  const _date = document.createElement("span");
-  const _title = document.createElement("h2");
-  const seeMoreInfo = document.createElement("p");
-  const timelineImage = document.createElement("img");
-  const closeButton = document.createElement("span");
-  wrapper.setAttribute("id", "modal-container"); //<div id ="card-wrapper"></div>
-  timelineItemSummary.setAttribute("id", "modal-full-description");
-  cardHeader.setAttribute("id", "card-header");
-  timelineImage.setAttribute("src", image); //modal-image
-  timelineImage.setAttribute("id", "modal-title"); //<img src="image" alt="title"/>
-  closeButton.setAttribute("id", "modal-close-button");
-  const dateText = document.createTextNode(date);
-  const titleText = document.createTextNode(title);
-  const detailsText = document.createTextNode(fullDescription);
-  _title.appendChild(titleText);
-  _date.appendChild(dateText);
-  seeMoreInfo.appendChild(detailsText);
-  cardHeader.appendChild(_date);
-  cardHeader.appendChild(closeButton);
-  timelineItemSummary.appendChild(cardHeader);
-  timelineItemSummary.appendChild(timelineImage);
-  timelineItemSummary.appendChild(_title);
-  timelineItemSummary.appendChild(seeMoreInfo);
-  wrapper.appendChild(timelineItemSummary);
-  closeButton.addEventListener(
-    "click",
-    (e) => {
-      e.preventDefault();
-      timeline.removeChild(wrapper);
-    },
-    false
-  );
-  wrapper.style.display = "flex";
-  timeline.prepend(wrapper);
-}
 dates.map(({ date, title, summary }, index) => {
   const item = document.createElement("div");
-  const _date = document.createElement("span");
-  const _title = document.createElement("h2");
+  const dateTag = document.createElement("span");
+  const titleTag = document.createElement("h2");
   const _caption = document.createElement("p");
   const circle = document.createElement("div");
   const button = document.createElement("button");
   const dateText = document.createTextNode(date);
   const titleText = document.createTextNode(title);
   const captionText = document.createTextNode(summary);
-  const buttonText = document.createTextNode("see more info");
+  const buttonText = document.createTextNode("see more info🤩");
   item.setAttribute("class", "timeline-item");
   button.setAttribute("data-index", index);
   circle.setAttribute("class", "timeline-circle");
-  _title.setAttribute("class", "timeline-item-title");
-  _date.setAttribute("class", "timeline-item-date");
+  titleTag.setAttribute("class", "timeline-item-title");
+  dateTag.setAttribute("class", "timeline-item-date");
   _caption.setAttribute("class", "timeline-item-summary");
   button.setAttribute("class", "timeline-item-more-info");
-  _title.appendChild(titleText);
-  _date.appendChild(dateText);
+  titleTag.appendChild(titleText);
+  dateTag.appendChild(dateText);
   _caption.appendChild(captionText);
   button.appendChild(buttonText);
   item.appendChild(circle);
-  circle.appendChild(_date);
-  item.appendChild(_title);
+  circle.appendChild(dateTag);
+  item.appendChild(titleTag);
   item.appendChild(_caption);
   item.appendChild(button);
   button.addEventListener(
@@ -75,9 +34,57 @@ dates.map(({ date, title, summary }, index) => {
     (e) => {
       e.preventDefault();
       const index = e.target.getAttribute("data-index");
-      if (index) getCard(index);
+      if (index) modal (index);
     },
     false
   );
   timeline.appendChild(item);
 });
+
+function modal (index) {
+  const { date, title, image, fullDescription } = dates[index];
+  const modalContainer = document.createElement("div");
+  const modalTitle = document.createElement("div");
+  const modalContent = document.createElement("div");
+
+  const dateTag = document.createElement("span");
+  const titleTag = document.createElement("h2");
+  const seeMoreInfo = document.createElement("p");
+  const imageTag = document.createElement("img");
+  const closeButton = document.createElement("span");
+
+  modalContainer.setAttribute("id", "modal-container");
+  modalContent.setAttribute("id", "modal-content");
+  modalTitle.setAttribute("id", "modal-title");
+  imageTag.setAttribute("src", image);
+  imageTag.setAttribute("alt", title);
+  closeButton.setAttribute("id", "modal-close-button");
+
+  const dateText = document.createTextNode(date);
+  const titleText = document.createTextNode(title);
+  const detailsText = document.createTextNode(fullDescription);
+
+  titleTag.appendChild(titleText);
+  dateTag.appendChild(dateText);
+  seeMoreInfo.appendChild(detailsText);
+
+  modalTitle.appendChild(dateTag);
+  modalTitle.appendChild(closeButton);
+  modalContent.appendChild(modalTitle);
+  modalContent.appendChild(imageTag);
+  modalContent.appendChild(titleTag);
+  modalContent.appendChild(seeMoreInfo);
+  modalContainer.appendChild(modalContent);
+
+  closeButton.addEventListener(
+    "click",
+    (e) => {
+      e.preventDefault();
+      timeline.removeChild(modalContainer);
+    },
+    false
+  );
+
+  modalContainer.style.display = "flex";
+  timeline.prepend(modalContainer);
+};
